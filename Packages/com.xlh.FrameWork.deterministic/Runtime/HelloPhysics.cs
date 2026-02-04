03d15e52119e834c9163ab034292e6aeeca25c5f
itcast <hello@itcast.cn>
2026-02-04 14:03:46 +0800

    1

---
+++ Packages/com.xlh.FrameWork.deterministic/Runtime/HelloPhysics.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace XLH.Physics
+{
+    public static class HelloPhysics
+    {
+        public static void SayHello()
+        {
+            Debug.Log("Hello from XLH Physics Package");
+        }
+    }
+}
